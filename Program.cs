019a2cd337182e42a20a11dc7f8085e2f6826604
Vita Chumakova <me@ezhevita.dev>
2024-06-26 02:38:45 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<ConditionalSelectBenchmark>();
+
+[DisassemblyDiagnoser]
+public class ConditionalSelectBenchmark
+{
+	private static readonly byte[] Mask = [255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0, 255, 0];
+
+	[Benchmark]
+	public byte ConditionalSelectField()
+	{
+		var result = Vector128.ConditionalSelect(Vector128.Create(Mask), Vector128<byte>.One, Vector128<byte>.Zero);
+
+		return result[0];
+	}
+
+	[Benchmark]
+	public byte BlendVariable()
+	{
+		var result = Sse41.BlendVariable(Vector128<byte>.One, Vector128<byte>.Zero, Vector128.Create(Mask));
+
+		return result[0];
+	}
+}
